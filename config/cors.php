8183a1102ca06d2887a0e90a7e1d61d12ead1a65
FernandoZocarato <103518777+FernandoZocarato@users.noreply.github.com>
2026-07-28 15:36:18 -0300

    Adiciona config/cors.php

---
+++ config/cors.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => array_filter([
+        env('FRONTEND_URL'),
+        'http://localhost:8000',
+        'http://127.0.0.1:8000',
+    ]),
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
